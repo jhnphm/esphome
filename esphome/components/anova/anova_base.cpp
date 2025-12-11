5d9d66269e73a93136f9f95a6521944ffd0df54c
John Pham <jhnphm@gmail.com>
2025-12-11 02:05:17 -0500

    Fix unit assignment logic in anova_base.cpp

--- esphome/components/anova/anova_base.cpp
+++ esphome/components/anova/anova_base.cpp
@@ -120,8 +120,8 @@ void AnovaCodec::decode(const uint8_t *data, uint16_t length) {
     }
     case SET_UNIT:
     case READ_UNIT: {
-      // this->unit_ = buf[0];
-      this->fahrenheit_ = this->fahrenheit_conf_ == 'f';
+      this->unit_ = buf[0];
+      this->fahrenheit_ = this->fahrenheit_ == 'f';
       this->has_unit_ = true;
       break;
     }
